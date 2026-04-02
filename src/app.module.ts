598b16e21c2d44633d6e0aa2d7af3b71f6d21375
sandenson <lgabrielsandes@gmail.com>
2026-04-02 11:51:25 -0300

    feat: .env module and validation implemented.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PG_DB_NAME: Joi.string().required(),
+        PG_PASSWORD: Joi.string().required(),
+        PG_USER: Joi.string().required(),
+        PG_PORT: Joi.number().port().default(5432),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
