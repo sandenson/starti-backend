4041853aafc266e16f1a64e5012b316fff598756
sandenson <lgabrielsandes@gmail.com>
2026-04-02 19:47:00 -0300

    feat: Swagger set up.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Starti backend')
+    .setDescription(
+      "App created for Starti's backend trainee technical examination.",
+    )
+    .setVersion('1.0')
+    .addTag('swag', 'this app is swagful')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (_: string, methodKey: string) => methodKey,
+  };
+
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, config, options);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 bootstrap();
