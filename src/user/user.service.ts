0eec6baf534573d355d38a1f1cb93adfd6b06765
sandenson <lgabrielsandes@gmail.com>
2026-04-03 21:07:47 -0300

    feat: UserService done.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Not, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    const { email, username, ...userData } = dto;
+    const foundUser = await this.usersRepository.findOneBy([
+      { email },
+      { username },
+    ]);
+
+    if (foundUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const newUser = this.usersRepository.create({
+      email,
+      username,
+      ...userData,
+    });
+    return this.usersRepository.save(newUser);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll(): Promise<User[]> {
+    const users = await this.usersRepository.find();
+    return users;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string, withDeleted: boolean = false): Promise<User> {
+    try {
+      const user = await this.usersRepository.findOne({
+        where: { id },
+        withDeleted,
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      return user;
+    } catch (e: any) {
+      if (e.name == 'QueryFailedError' && /uuid/im.test(e.message)) {
+        throw new BadRequestException("ID isn't a valid UUID");
+      }
+
+      throw e;
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, dto: UpdateUserDto): Promise<User> {
+    const { email, username } = dto;
+
+    const user = await this.findOne(id);
+
+    if (email || username) {
+      const exists = await this.usersRepository.findOneBy([
+        email && { id: Not(id), email },
+        username && { id: Not(id), username },
+      ]);
+
+      if (email && exists?.email === email) {
+        throw new BadRequestException('Email already in use');
+      } else if (username && exists?.username === username) {
+        throw new BadRequestException('Username already in use');
+      }
+    }
+
+    console.log('oxe');
+
+    const updated = this.usersRepository.merge(user, dto);
+    return this.usersRepository.save(updated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async softRemove(id: string): Promise<User> {
+    const user = await this.findOne(id);
+    return this.usersRepository.softRemove(user);
+  }
+
+  async remove(id: string): Promise<User> {
+    const user = await this.findOne(id);
+    return this.usersRepository.remove(user);
   }
 }
