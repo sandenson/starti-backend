9617d33facd6a52d0de2786615d4899dba23021b
sandenson <lgabrielsandes@gmail.com>
2026-04-04 18:41:08 -0300

    feat: Post DTOs properly created.

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePostDto {}
+import { ToString, Trim } from '@buka/class-transformer-extra';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { randomUUID } from 'crypto';
+import { lipsumParagraph } from 'src/utils';
+
+export class CreatePostDto {
+  @Trim()
+  @IsUUID()
+  @ApiProperty({
+    description: 'UUID of the user who created the post',
+    example: randomUUID(),
+  })
+  userId: string;
+
+  @ToString({ optional: true })
+  @IsString()
+  @IsNotEmpty()
+  @Trim()
+  @ApiProperty({
+    description: 'Text content of the post',
+    example: lipsumParagraph,
+  })
+  text: string;
+}
--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/swagger';
+import { ToBoolean } from '@buka/class-transformer-extra';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(CreatePostDto) {
+  @IsOptional()
+  @ToBoolean(undefined, { optional: true })
+  @IsBoolean()
+  @ApiProperty({
+    description: 'Whether the post is archived or not',
+    required: false,
+    example: false,
+  })
+  archived?: boolean;
+}
