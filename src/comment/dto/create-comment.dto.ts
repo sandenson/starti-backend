3c09684f58e716f44930badedb0907674a60c48d
sandenson <lgabrielsandes@gmail.com>
2026-04-04 21:54:41 -0300

    feat: Create comment create and update DTOs.

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,1 +1,34 @@
-export class CreateCommentDto {}
+import { ToString, Trim } from '@buka/class-transformer-extra';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, IsUUID, MaxLength } from 'class-validator';
+import { randomUUID } from 'crypto';
+import { lipsumParagraph } from 'src/utils';
+
+export class CreateCommentDto {
+  @Trim()
+  @IsUUID()
+  @ApiProperty({
+    description: 'UUID of the user who posted the comment',
+    example: randomUUID(),
+  })
+  userId: string;
+
+  @Trim()
+  @IsUUID()
+  @ApiProperty({
+    description: 'UUID of the post the comment was made in',
+    example: randomUUID(),
+  })
+  postId: string;
+
+  @ToString({ optional: true })
+  @IsString()
+  @Trim()
+  @IsNotEmpty()
+  @MaxLength(5000)
+  @ApiProperty({
+    description: 'Text content of the comment',
+    example: lipsumParagraph,
+  })
+  message: string;
+}
