538c41b1e6b675f89732368067e34510aef87fda
sandenson <lgabrielsandes@gmail.com>
2026-04-04 21:50:47 -0300

    fix: Prevent changing userId of post via update DTO.

--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,9 +1,11 @@
 import { ToBoolean } from '@buka/class-transformer-extra';
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
 import { CreatePostDto } from './create-post.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {
+export class UpdatePostDto extends PartialType(
+  OmitType(CreatePostDto, ['userId']),
+) {
   @IsOptional()
   @ToBoolean(undefined, { optional: true })
   @IsBoolean()
